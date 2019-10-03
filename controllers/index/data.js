0edf48a5fee178676c0a8df6d82b6204c6fc1733
gonzalojs <eogutier@gmail.com>
2019-10-02 21:34:08 -0300

    listo for each

--- controllers/index/data.js
+++ controllers/index/data.js
@@ -1,3 +1,8 @@
 exports.data_post = (req, res) => {
   console.log(req.body[0])
+
+  for (let index = 0; index < req.body.length; index++) {
+    const element = req.body[index];
+    console.log(element)
+  }
 }
