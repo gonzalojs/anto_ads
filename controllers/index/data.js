703691a20805a08fd6f7e5c84a3e8fdd8ef6a2d9
gonzalojs <eogutier@gmail.com>
2019-10-04 19:20:48 -0300

    remove new lines

--- controllers/index/data.js
+++ controllers/index/data.js
@@ -2,18 +2,20 @@ const Ad = require('../../models/ad.model')
 const { Types } = require('mongoose')
 
 exports.data_post = (req, res) => {
-  console.log(req.body[0])
+
+
 
   for (let index = 0; index < req.body.length; index++) {
     const element = req.body[index];
     const reg = /([^. — ]+)/
-    let tit = reg.exec(Object.values(element))[0] //titulo
+/*     let tit = reg.exec(Object.values(element))[0] //titulo */
     let body = Object.values(element)[0]
+/*     let otr = Object.values(element)[0].replace(tit, '') */
     /* console.log(Object.values(element)[0].replace(tit, '')) */
 /*     console.log(typeof body) */
 
     if (typeof body === 'string') {
-      console.log(Object.values(element)[0].replace(tit, ''))
+      console.log(body.replace(/\r?\n|\r/g, ''))
     } else {
       console.log('no string')
     }
