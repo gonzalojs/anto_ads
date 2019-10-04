19388d7fc6aa2293af2af64a78a45213193bbed1
gonzalojs <eogutier@gmail.com>
2019-10-03 21:18:29 -0300

    object values

--- controllers/index/data.js
+++ controllers/index/data.js
@@ -1,8 +1,14 @@
+const Ad = require('../../models/ad.model')
+const { Types } = require('mongoose')
+
 exports.data_post = (req, res) => {
   console.log(req.body[0])
 
   for (let index = 0; index < req.body.length; index++) {
     const element = req.body[index];
-    console.log(element)
+    console.log(Object.values(element))
+
+
+
   }
 }
