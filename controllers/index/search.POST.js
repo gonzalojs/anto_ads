9ec824f11d4d5c7e0581c1d0a93b44f586f51966
gonzalojs <eogutier@gmail.com>
2019-10-22 22:18:59 -0300

    split

--- controllers/index/search.POST.js
+++ controllers/index/search.POST.js
@@ -2,7 +2,7 @@ exports.search_post = (req, res) => {
   const body = req.body.body
 
   if (body.length > 0) {
-    console.log('este cuerpo tiene body')
+    console.log(body.split(' ').length)
     res.redirect('/search')
   } else {
     console.log('esto está pelado')
