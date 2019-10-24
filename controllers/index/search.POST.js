ff125f971ada71b48b4e619facb5c251084517eb
gonzalojs <eogutier@gmail.com>
2019-10-23 23:04:49 -0300

    join

--- controllers/index/search.POST.js
+++ controllers/index/search.POST.js
@@ -2,7 +2,10 @@ exports.search_post = (req, res) => {
   const body = req.body.body
 
   if (body.length > 0) {
-    console.log(body.split(' ').length)
+    let sp = body.split(' ')
+    let param = 'p='
+    let joined = sp.join(param)
+    console.log(joined)
     res.redirect('/search')
   } else {
     console.log('esto está pelado')
