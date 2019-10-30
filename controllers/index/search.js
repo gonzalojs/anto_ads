dc993f453fc9190f80ca7d5b2a1024d82391f7f0
gonzalojs <eogutier@gmail.com>
2019-10-30 16:09:27 -0300

    new brach pull

--- controllers/index/search.js
+++ controllers/index/search.js
@@ -3,13 +3,16 @@ const Ad = require('../../models/ad.model')
 exports.search = (req, res) => {
 
   console.log(req.query)
+  let query = []
+
 
   Ad.find({
     $text: {
-      $search: req.query.p[0]
+      $search: req.query.p[0] //Esto solo busca lo primero
     }
   })
   .then((result) => {
+    query.push(result)
     res.render('search', {
       body: result
     })
