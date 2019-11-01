f944b6ce6d34f19e6f5554b3e7ca1e7be7a5eac0
gonzalojs <eogutier@gmail.com>
2019-10-31 22:18:46 -0300

    query

--- controllers/index/search.js
+++ controllers/index/search.js
@@ -1,25 +1,14 @@
 const Ad = require('../../models/ad.model')
 
 exports.search = (req, res) => {
-<<<<<<< HEAD
+
   const query = req.query.p.join()
 
   Ad.find({
     $text: {
       $search: query
-=======
-
-  console.log(req.query)
-  let query = []
-
-
-  Ad.find({
-    $text: {
-      $search: req.query.p[0] //Esto solo busca lo primero
->>>>>>> 010712222aaecfa33031bb560a2ca7d02a8c2ecb
-    }
-  })
-  .then((result) => {
+    }})
+    .then((result) => {
     query.push(result)
     res.render('search', {
       body: result
