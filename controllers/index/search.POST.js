ddb53aef7599661fe51de5d5f07db0b792005677
gonzalojs <eogutier@gmail.com>
2019-11-01 22:34:08 -0300

    search

--- controllers/index/search.POST.js
+++ controllers/index/search.POST.js
@@ -5,7 +5,7 @@ exports.search_post = (req, res) => {
     let sp = body.split(' ')
     let param = '&p='
     let joined = sp.join(param)
-    res.redirect(`/search?p=${joined}`)
+    res.redirect(`/search?p=${joined}`) // redirecto to get search + los params
   } else {
     console.log('esto está pelado')
   }
