1be6ac1256b738a48cf170ca46659e95ae65a223
gonzalojs <eogutier@gmail.com>
2019-11-03 21:40:46 -0300

    options ready

--- routes/anto.routes.js
+++ routes/anto.routes.js
@@ -7,9 +7,11 @@ const antoLoginPostController = require('../controllers/anto/login.post')
 const antoLoginGetController = require('../controllers/anto/login.get')
 const antoIndexController = require('../controllers/anto/index')
 const antoUploadController = require('../controllers/anto/upload')
+const antoOptionsController = require('../controllers/anto/options')
 
 router.get('/', antoIndexController.anto_index)
 router.get('/upload', antoUploadController.anto_upload)
+router.get('/options', antoOptionsController.anto_options)
 
 router.get('/register', antoRegisterGetController.register_get)
 router.post('/register', antoRegisterPostController.register_post)
