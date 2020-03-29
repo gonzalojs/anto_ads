67310a8723aa701bfef1ea7fc482ad69b12fa248
gonzalojs <eogutier@gmail.com>
2020-03-28 21:09:58 -0300

    server time out

--- server.js
+++ server.js
@@ -5,3 +5,4 @@ const port = process.env.PORT || 3500
 
 const server = http.createServer(app)
 server.listen(port, () => console.log(`Corriendo en el puerto ${port}`))
+server.keepAliveTimeout = 61 * 1000;
